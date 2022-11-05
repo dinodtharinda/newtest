f3ed742f52422710d29c46bc8f5cd8166b46f73c
dinodtharinda <94352564+dinodtharinda@users.noreply.github.com>
2022-11-05 09:28:58 +0530

    reduce home view code

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -15,13 +15,77 @@ class HomePage extends StatefulWidget {
 enum MenuAction { logout, more }
 
 class _HomePageState extends State<HomePage> {
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+//show message dialog
+  Future<bool> showMessageDialog() {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('LogOut'),
+            content: const Text('Are you sure you want to Log Out'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text(
+                  'Logout',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+            ],
+          );
+        }).then((value) => value ?? false);
+  }
+//Popup Menu Button
+  Widget popUpMenu() {
+    return PopupMenuButton<MenuAction>(
+        icon: const Icon(
+          Icons.more_vert_sharp,
+          color: Colors.black,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        onSelected: (value) async {
+          switch (value) {
+            case MenuAction.logout:
+              final shouldLogout = await showMessageDialog(); //call show message dialog widgit
+              devtools.log(shouldLogout.toString());
+              if (shouldLogout) {
+                await Firebase.initializeApp(
+                    options: DefaultFirebaseOptions.currentPlatform);
+                await FirebaseAuth.instance.signOut();
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil('/login/', (route) => false);
+              }
+              break;
+            case MenuAction.more:
+              break;
+          }
+        },
+        itemBuilder: (context) {
+          return const [
+            PopupMenuItem<MenuAction>(
+              value: MenuAction.logout,
+              child: Text('Logout'),
+            ),
+            PopupMenuItem<MenuAction>(
+              value: MenuAction.more,
+              child: Text('more'),
+            ),
+          ];
+        });
+  }
+// AppBar
+AppBar appbar(){
+    return AppBar(
         title: const Text(
           'HOME',
           style: TextStyle(color: Colors.black),
@@ -29,76 +93,18 @@ class _HomePageState extends State<HomePage> {
         elevation: 0,
         backgroundColor: Colors.transparent,
         actions: [
-          PopupMenuButton<MenuAction>(
-              icon: const Icon(
-                Icons.more_vert_sharp,
-                color: Colors.black,
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              onSelected: (value) async {
-                switch (value) {
-                  case MenuAction.logout:
-                    final shouldLogout = await showMessageDialog(context);
-                    devtools.log(shouldLogout.toString());
-                    if (shouldLogout) {
-                      await Firebase.initializeApp(
-                          options: DefaultFirebaseOptions.currentPlatform);
-                      await FirebaseAuth.instance.signOut();
-                      Navigator.of(context)
-                          .pushNamedAndRemoveUntil('/login/', (route) => false);
-                    }
-                    break;
-                  case MenuAction.more:
-                    break;
-                }
-              },
-              itemBuilder: (context) {
-                return const [
-                  PopupMenuItem<MenuAction>(
-                    value: MenuAction.logout,
-                    child: Text('Logout'),
-                  ),
-                  PopupMenuItem<MenuAction>(
-                    value: MenuAction.more,
-                    child: Text('more'),
-                  ),
-                ];
-              })
+          popUpMenu(),//cal PopUpMenu
         ],
-      ),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appbar(),//call App widgit
       body: const Center(
         child: Text('Home'),
       ),
     );
   }
-}
-
-Future<bool> showMessageDialog(BuildContext context) {
-  return showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('LogOut'),
-          content: const Text('Are you sure you want to Log Out'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(false);
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(true);
-              },
-              child: const Text(
-                'Logout',
-                style: TextStyle(color: Colors.red),
-              ),
-            ),
-          ],
-        );
-      }).then((value) => value ?? false);
 }
